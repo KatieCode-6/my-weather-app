7b8db80bb5c2970ca4e288b4b51f9ac4aaabbca4
KatieCode-6 <katrinkovacova@gmial.com>
2021-01-13 17:27:35 +0100

    Added URL for GitHub

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App"> Weather App
-      </div>;
+  return <div className="App"> 
+  <h1>Weather App</h1>
+  <footer>
+    This project is created by
+     <a href="https://www.katiekovacova.co.uk/education.html" target="_blank"> Katie Kovacova </a>
+    and is {" "}   
+  <a href ="https://github.com/KatieCode-6/my-weather-app"
+  target="_blank" >
+    open-sources on GitHub
+  </a>
+  </footer>
+  </div>;
 }
 
 
