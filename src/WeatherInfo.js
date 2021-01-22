c161affcb1050a817bd60a082c778cf7e127ff53
KatieCode-6 <katrinkovacova@gmial.com>
2021-01-22 13:32:40 +0100

    Added unit Conversion celsius and fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,9 +1,11 @@
-import react from "react";
+import React from "react";
 import CurrentDate from "./CurrentDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return ( 
-        <div className= "weather-info"> 
+        <div className= "WeatherInfo"> 
     <h1>{props.data.city}</h1>
          
         <ul>
@@ -15,21 +17,24 @@ export default function WeatherInfo(props) {
         </ul>
     
         <div className="row mt-3">
-            <div className="col-6">
-                <img src= {props.data.iconUrl}
-                alt={props.data.descripton} height="70px" width="70px" />
-                <span className="temperature">{props.data.temperature}</span>
-                <span className="unit">℃</span>
+        <div className="col-6">
+        <div className="clearfix">
+        <div className="float-left">    
+        <WeatherIcon code={props.data.icon} />
+         </div>
+                    
+        <div className="float-left">
+            <WeatherTemperature celsius={props.data.temperature} />
         </div>
+     </div>
+   </div>
         <div className="col-6">
             <ul>
                 <li>Humidity: {props.data.humidity} %</li>
                 <li>Wind: {props.data.wind} km/h</li>
             </ul>
-
+          </div>
         </div>
-
-        </div>
-        </div>
+    </div>
     );
 }
